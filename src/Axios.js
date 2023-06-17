349343becba987769708953c9262150263b00467
Vishal Soni <vs332837@gmail.com>
2023-06-17 18:43:42 +0530

    updated

--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const Axios = axios.create({
-    baseURL: "http://localhost:5000",
+    // baseURL: "http://localhost:5000",
+    baseURL: "https://thevsoni-ecommerce-backen.onrender.com",
 })
 
 export default Axios;
